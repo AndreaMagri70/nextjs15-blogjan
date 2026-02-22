9da3e5a08adeb8fe2b3773eb8022753a578b6af3
AndreaMagri70 <assomag@libero.it>
2026-02-22 19:38:26 +0100

    Fix: middleware

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,13 @@
-export default function DashboardRoute() {
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardRoute() {
+  const {getUser} = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user) {
+    return redirect("/api/auth/register")
+  }
   return (
     <div>
         <h1>Hello from the Dashboard</h1>      
